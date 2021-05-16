94e7486e96362da340a6a111f41c8f01db23586b
goodPointP <49491425+goodPointP@users.noreply.github.com>
2021-05-16 04:13:18 -0700

    Outline of image scraping

--- imageScraping/imageScrape.py
+++ imageScraping/imageScrape.py
@@ -1,1 +1,24 @@
+def downloadHTML(ImdbID):
+  return html
 
+def extractImageLink(html):
+  return imageLink
+
+def downloadImage(link):
+  return image
+
+def saveImage(image):
+  try:
+    # save image
+    return True
+  except:
+    print('An error occured')
+    return False
+ 
+
+ImdbID = 'tt0120737'
+
+html = downloadHTML(ImdbID)
+imageLink = extractImageLink(html)
+image = downloadImage(imageLink)
+saveImage(image)
