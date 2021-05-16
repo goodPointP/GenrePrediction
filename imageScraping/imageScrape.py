ffc32a245c3aa0106fd25d62636d3c26fb1d0109
goodPointP <49491425+goodPointP@users.noreply.github.com>
2021-05-16 04:37:28 -0700

    Added overview code, not tested!

--- imageScraping/imageScrape.py
+++ imageScraping/imageScrape.py
@@ -1,24 +1,39 @@
+import urllib.request
+from bs4 import BeautifulSoup as BS
+
 def downloadHTML(ImdbID):
+  url = 'https://www.imdb.com/title/'+str(ImdbID)+'/'
+  response = urllib.request.urlopen(url)
+  html = response.read()
   return html
 
 def extractImageLink(html):
+  soup = BS(html #on_duplicate_attribute='ignore')
+  imageLink = soup.find('img')['src']
+  #OR
+  imageLink = soup.img['src']
   return imageLink
 
 def downloadImage(link):
+  response = requests.get(link)
+  image = response.content
   return image
 
-def saveImage(image):
+def saveImage(image, ImdbID):
+  fileName = str(ImdbID) + '.jpg'
+  fullPath = 'images/' + fileName
   try:
-    # save image
+    with open(fullPath, "w") as f:
+      f.write(image)
     return True
   except:
-    print('An error occured')
+    print(f'Did not save the image {fileName} to {fullPath}.')
     return False
  
-
+# test data
 ImdbID = 'tt0120737'
 
 html = downloadHTML(ImdbID)
 imageLink = extractImageLink(html)
 image = downloadImage(imageLink)
-saveImage(image)
+saveImage(image, ImdbID)
