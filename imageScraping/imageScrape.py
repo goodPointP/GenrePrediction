99846f14f5e4fa5303d88bc3b68912270a5f5e4e
goodPointP <lionnose@gmail.com>
2021-05-16 17:40:49 +0200

    [FINISHED] image scraping

--- imageScraping/imageScrape.py
+++ imageScraping/imageScrape.py
@@ -1,5 +1,7 @@
 import urllib.request
+import requests
 from bs4 import BeautifulSoup as BS
+import shutil
 
 def downloadHTML(ImdbID):
   url = 'https://www.imdb.com/title/'+str(ImdbID)+'/'
@@ -15,25 +17,31 @@ def extractImageLink(html):
   return imageLink
 
 def downloadImage(link):
-  response = requests.get(link)
-  image = response.content
-  return image
+  response = requests.get(link, stream = True)
+  if response.status_code == 200:
+    response.raw.decode_content = True
+    return response
 
 def saveImage(image, ImdbID):
   fileName = str(ImdbID) + '.jpg'
   fullPath = 'posters/' + fileName
   try:
-    with open(fullPath, "w") as f:
-      f.write(image)
+    with open(fullPath, "wb") as f:
+      shutil.copyfileobj(image.raw, f)
     return True
   except:
     print(f'Did not save the image {fileName} to {fullPath}.')
     return False
- 
-# test data
-ImdbID = 'tt0120737'
 
-html = downloadHTML(ImdbID)
-imageLink = extractImageLink(html)
-image = downloadImage(imageLink)
-saveImage(image, ImdbID)
+def getImages(ImdbIDs):
+    for ImdbID in ImdbIDs:
+        html = downloadHTML(ImdbID)
+        imageLink = extractImageLink(html)
+        image = downloadImage(imageLink)
+        saveImage(image, ImdbID)
+
+# test data
+# ImdbID = 'tt0120737'
+
+ids = ['tt0084302', 'tt0000003', 'tt0383846', 'tt0167260', 'tt2221420', 'tt3155794']
+getImages(ids)
