88448ad9eaa7df32f812340df7be0d4a7720a2e2
goodPointP <lionnose@gmail.com>
2021-05-18 17:58:06 +0200

    Added scraped images in high resolution

--- imageScraping/imageScrape.py
+++ imageScraping/imageScrape.py
@@ -1,4 +1,5 @@
 import urllib.request
+import re
 import requests
 from bs4 import BeautifulSoup as BS
 import shutil
@@ -11,17 +12,36 @@ def downloadHTML(ImdbID):
   html = response.read()
   return html
 
+def downloadMediaviewerHTML(mediaViewerlink):
+  response = urllib.request.urlopen(mediaViewerlink)
+  return response
+
 def extractImageLink(html):
   soup = BS(html) #on_duplicate_attribute='ignore')
-  imageLink = soup.find('img')['src']
-  #OR
-  imageLink = soup.img['src']
-  return imageLink
+  posterDIV = soup.find_all('div', {'class': 'poster'})
+  match = re.search('<a href="(.*)">', str(posterDIV))
+  posterDIV = match.group(1)
 
-def downloadImage(link):
+  mediaViewerlink = 'https://www.imdb.com'+posterDIV
+  mediaViewerHTML = downloadMediaviewerHTML(mediaViewerlink)
+  soup = BS(mediaViewerHTML)
+  imageLink = soup.find_all('div', {'style': 'max-height:493px;max-width:600px;left:calc(50% + 0px)'})
+  print(f'Image link: {str(imageLink)}\n')
+  match = re.search(' src="(.*?)"', str(imageLink)).group(1)
+  # imageLink = soup.find_all('div', {'class': 'poster'})
+  #OR
+  # imageLink = soup.img['src']
+  return match
+
+def downloadAndSaveImage(link, ImdbID):
+  print(f"Link: {link}")
+  fileName = str(ImdbID) + '.jpg'
+  fullPath = 'posters/' + fileName
   response = requests.get(link, stream = True)
   if response.status_code == 200:
     response.raw.decode_content = True
+    with open(fullPath, "wb") as f:
+      shutil.copyfileobj(response.raw, f)
     return response
 
 def saveImage(image, ImdbID):
@@ -33,25 +53,27 @@ def saveImage(image, ImdbID):
     return True
   except:
     print(f'Did not save the image {fileName} to {fullPath}.')
-    return False
+    return image
 
 def getImages(ImdbIDs):
     for ImdbID in ImdbIDs:
+        print(ImdbID)
         html = downloadHTML(ImdbID)
         imageLink = extractImageLink(html)
-        image = downloadImage(imageLink)
-        saveImage(image, ImdbID)
+        downloadAndSaveImage(imageLink, ImdbID)
+        # image = downloadAndSaveImage(imageLink)
+        # saveImage(image, ImdbID)
 
 # test data
-# ids = ['tt0084302', 'tt0000003', 'tt0383846', 'tt0167260', 'tt2221420', 'tt3155794']
-# getImages(ids)
+ids = ['tt0000003', 'tt0084302', 'tt0383846', 'tt0167260', 'tt2221420', 'tt3155794']
+a = getImages(ids)
 
 #%%
-if __name__ == '__main__':
-    data = pd.read_csv("../data/IMDb_movies.csv")
-    df = data.drop(['original_title', "date_published", "language", "votes", "actors", "director", "writer", "production_company", "metascore", "reviews_from_users", "reviews_from_critics"], axis = 1).dropna()
-    IDs = df.imdb_title_id
-    # start getting images
-    # getImages(IDs)
-    with Pool(15) as p:
-        p.map(getImages, IDs)
+
+data = pd.read_csv("../data/IMDb_movies.csv")
+# df = data.drop(['original_title', "date_published", "language", "votes", "actors", "director", "writer", "production_company", "metascore", "reviews_from_users", "reviews_from_critics"], axis = 1).dropna()
+IDs = data.imdb_title_id
+
+#%%
+# start getting images
+getImages(IDs)
