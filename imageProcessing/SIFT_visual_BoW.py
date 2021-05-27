09dc1c8ba891477245fcb251b34709daaa9eb75e
goodPointP <49491425+goodPointP@users.noreply.github.com>
2021-05-27 06:39:04 -0700

    Fixed SIFT

--- imageProcessing/SIFT_visual_BoW.py
+++ imageProcessing/SIFT_visual_BoW.py
@@ -1,20 +1,38 @@
+from sklearn.decomposition import PCA
 import cv2
-from sklearn.cluster import KMeans, build_histogram
+import numpy as np
+from sklearn.cluster import MiniBatchKMeans
+import itertools
+import pandas as pd
 
-# defining feature extractor that we want to use
-extractor = cv2.xfeatures2d.SIFT_create()
+#%%
+import pickle
+with open('../data/dataset_final.pkl', 'rb') as f:
+    data = pickle.load(f)
+IDs = data.imdb_title_id
 
-def features(image, extractor):
-    keypoints, descriptors = extractor.detectAndCompute(image, None)
-    return keypoints, descriptors
+#%%
+# training_paths = ['tt9825006.jpg', 'tt9779516.jpg', 'tt9426210.jpg']
+sift = cv2.SIFT_create()
+siftResults = []
 
-kmeans = KMeans(n_clusters = 800)
-kmeans.fit(descriptor_list)
+for ID in IDs:
+    image = cv2.imread('posters/highres/' + ID + '.jpg')
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    kp, dsc = sift.detectAndCompute(gray, None)
+    tempResults = list(zip([kps.response for kps in kp], dsc))
+    tempResultsSorted = sorted (tempResults, key = lambda x: x[0])[-10:]
+    
+    sortedSIFTDescs = []
+    for row in tempResultsSorted:
+        sortedSIFTDescs.append(row[1])
+    
+    merged = np.array(list(itertools.chain.from_iterable(sortedSIFTDescs)))
+    siftResults.append(merged)
 
-preprocessed_image = []
-for image in images:
-      image = gray(image)
-      keypoint, descriptor = features(image, extractor)
-      if (descriptor is not None):
-          histogram = build_histogram(descriptor, kmeans)
-          preprocessed_image.append(histogram)
+siftResults = np.array(siftResults)
+
+#%%
+import pickle
+with open('imageFeaturesOnlySIFT.pkl', 'wb') as outfile:
+    pickle.dump(siftResults, outfile)
